cbd79cb1007ea2105bb0f635a0f75568885ad7ee
Brandon-Magofna <magofna68@gmail.com>
2021-05-28 09:26:50 -0700

    :sparkles: add first migration

--- Models/DesignTimeDbContextFactory.cs
+++ Models/DesignTimeDbContextFactory.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkDesign;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
@@ -10,14 +10,14 @@ namespace Factory.Models
     FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrectDirectory())
-        .AddJsonFile("appsettings.json")
-        .Build();
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
 
       var builder = new DbContextOptionsBuilder<FactoryContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"],
-      ServerVersion.AutoDetect(configuration["ConnectionsStrings:DefaultConnection"]));
+      ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
       return new FactoryContext(builder.Options);
     }
